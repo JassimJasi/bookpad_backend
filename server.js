dc3b1ecda62664bb8da6fc4e4d441938fb175109
JassimJasi <jassimsadanamks@gmail.com>
2022-12-17 14:13:09 +0530

    cors made to allow all

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const fileUpload = require("express-fileupload");
 
 const { readdirSync } = require("fs");
 const app = express();
-// const options = {
-//   origin: "http://localhost:3000",
-//   useSuccessStatus: 200,
-// };
+const options = {
+  origin: "http://localhost:3000",
+  useSuccessStatus: 200,
+};
 app.use(express.json());
-app.use(cors());
+app.use(cors({ options }));
 app.use(
   fileUpload({
     useTempFiles: true,
